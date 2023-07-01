45dd684073ad49a87ce0ab3f68f2da5cf56abd78
Gabriel <gabrielsantana2267@gmail.com>
2023-07-01 13:45:43 -0300

    :bug: fix: Adjust Project Section

--- portfolio/src/components/Main/section/project/index.jsx
+++ portfolio/src/components/Main/section/project/index.jsx
@@ -3,6 +3,7 @@ import './project.css'
 import icon_alfa from '../../../../assets/alfa_icon.png'
 import icon_viagem from '../../../../assets/viagem_icon.png'
 import todo from '../../../../assets/todo.png'
+import blogr from '../../../../assets/blogicon.png'
 
 export const Project = () => {
     return (
@@ -12,6 +13,8 @@ export const Project = () => {
                 <Card name="AlfaTech" description="Projeto feito utilizando HTML e CSS" link="https://alfa-tech.vercel.app" img={icon_alfa}/>
                 <Card name="Viagens" description="Projeto feito utilizando HTML e CSS" link="https://projeto-one-pi.vercel.app" img={icon_viagem} />
                 <Card name="ToDo - List" description="Projeto feito utilizando HTML, CSS, JavaScript" link="https://to-do-list-smoky-alpha.vercel.app/#" img={todo} />
+                <Card name="Blogr" description="Projeto feito utilizando Typescript e Styled Component" link="https://blogr-five-kappa.vercel.app" img={blogr} />
+                
             </div>
         </div>
     )
